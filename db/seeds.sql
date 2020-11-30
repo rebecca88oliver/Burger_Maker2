9ec57ceebb3d7f30e13cca9100acfdbfed3038aa
Rebecca88 <rebecca88oliver@gmail.com>
2020-11-30 15:01:03 -0500

    seeds & sql
    initial uploads and added db file, mysql schema, and seeds.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Maple Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ghost Pepper Burger');
